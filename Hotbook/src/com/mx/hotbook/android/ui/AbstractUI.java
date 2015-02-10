73c8bcc16645dd5b0a95e3dd6158d1157dc74200
DeliriumTremens <daniel.castillo00@gmail.com>
2015-02-10 17:19:41 -0600

    se integra la utileria Calligraphy

--- Hotbook/src/com/mx/hotbook/android/ui/AbstractUI.java
+++ Hotbook/src/com/mx/hotbook/android/ui/AbstractUI.java
@@ -1,14 +1,32 @@
 package com.mx.hotbook.android.ui;
 
+import com.mx.hotbook.android.R;
+import com.mx.hotbook.android.util.calligraphy.CalligraphyConfig;
+import com.mx.hotbook.android.util.calligraphy.CalligraphyContextWrapper;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 public abstract class AbstractUI extends Activity{
 	
   protected String TAG = "HotBox";
 	
+  static{
+	  CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                       .setDefaultFontPath("fonts/SohoStd-MediumCondensed.otf")
+                       .setFontAttrId(R.attr.fontPath)
+                       .build()
+      );
+  }
+  
   public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
   }
 
+  @Override
+  protected void attachBaseContext(Context newBase) {
+      super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+  }
+  
 }
